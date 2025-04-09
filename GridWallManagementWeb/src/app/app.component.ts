f9fbf1a12958b7b383ec72a977e8bbbc087e3ea6
kishan.infiadd <kishan.infiadd@gmail.com>
2025-04-09 10:39:59 +0530

    miss changes

--- GridWallManagementWeb/src/app/app.component.ts
+++ GridWallManagementWeb/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { ThemeService } from './services/theme/theme.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    standalone: false
 })
 export class AppComponent {
   title = 'GridWallManagementWeb';
--- GridWallManagementWeb/src/app/shared/components/responsive-helper/responsive-helper.component.ts
+++ GridWallManagementWeb/src/app/shared/components/responsive-helper/responsive-helper.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 
 @Component({
-  selector: 'app-responsive-helper',
-  templateUrl: './responsive-helper.component.html',
-  styleUrl: './responsive-helper.component.scss',
+    selector: 'app-responsive-helper',
+    templateUrl: './responsive-helper.component.html',
+    styleUrl: './responsive-helper.component.scss',
+    standalone: false
 })
 export class ResponsiveHelperComponent implements OnInit {
   public env: any = environment;
