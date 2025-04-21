6df1add6b8fedf6117f1a476d3fa1023a430fd9f
kishan.infiadd <kishan.infiadd@gmail.com>
2025-04-21 15:23:28 +0530

    component added

--- GridWallManagement.Web/src/app/core/core.module.ts
+++ GridWallManagement.Web/src/app/core/core.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { CoreComponent } from './core.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { SidebarComponent } from './shared/layout/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [CoreComponent],
+  declarations: [
+    CoreComponent,
+    SidebarComponent,
+    FooterComponent,
+    HeaderComponent,
+  ],
   imports: [CommonModule, CoreRoutingModule],
 })
 export class CoreModule {}
